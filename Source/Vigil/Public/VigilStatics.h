9fa9ff6d1dcd0514998b2c623137b65667ee1575
Jared Taylor <jaredtaylor.99@gmail.com>
2025-04-21 19:15:17 +1200

    Remove old code

--- Source/Vigil/Public/VigilStatics.h
+++ Source/Vigil/Public/VigilStatics.h
@@ -8,11 +8,6 @@
 
 #include "VigilStatics.generated.h"
 
-namespace EDrawDebugTrace
-{
-	enum Type : int;
-}
-
 struct FScalableFloat;
 
 /**
@@ -43,26 +38,5 @@ public:
 		float Thickness = 0.f, uint8 DepthPriority = 0);
 
 	UFUNCTION(BlueprintPure, Category=Vigil)
-	static FVector GetConeBoxShapeExtent(FVigilConeShape Cone);
-	
-	// /**
-	//  * This is a debugging function for Vigil to ensure that the MakeConeBoxShape works as expected
-	//  * It is actually just a box trace
-	//  */
-	// UFUNCTION(BlueprintCallable, Category="Collision", meta=(bIgnoreSelf="true", WorldContext="WorldContextObject", AutoCreateRefTerm="ActorsToIgnore", DisplayName = "Multi Cone Trace By Channel", AdvancedDisplay="TraceColor,TraceHitColor,DrawTime", Keywords="sweep"))
-	// static bool ConeBoxTraceMulti(const UObject* WorldContextObject, const FVector Start, const FVector End,
-	// 	float Length, float AngleWidth, float AngleHeight, FRotator Orientation, ETraceTypeQuery TraceChannel, bool bTraceComplex,
-	// 	const TArray<AActor*>& ActorsToIgnore, EDrawDebugTrace::Type DrawDebugType, TArray<FHitResult>& OutHits,
-	// 	bool bIgnoreSelf, FLinearColor TraceColor = FLinearColor::Red, FLinearColor TraceHitColor = FLinearColor::Green,
-	// 	float DrawTime = 5.0f);
-
-	/**
-	* Performs a box trace and draws the cone shape
-	* For debugging only, results will never be useful
-	*/
-	// UFUNCTION(BlueprintCallable, Category = "Collision", meta = (DevelopmentOnly, bIgnoreSelf = "true", WorldContext="WorldContextObject", AutoCreateRefTerm = "ActorsToIgnore", DisplayName = "Multi Cone Trace By Channel", AdvancedDisplay="TraceColor,TraceHitColor,DrawTime", Keywords="sweep"))
-	static bool VigilConeTraceDebug(const UObject* WorldContextObject, const FVector Start, const FVector InEnd, FVigilConeShape Cone,
-		const FRotator Orientation, ETraceTypeQuery TraceChannel, bool bTraceComplex, const TArray<AActor*>& ActorsToIgnore,
-		TArray<FHitResult>& OutHits, bool bIgnoreSelf, FLinearColor TraceColor = FLinearColor::Red,
-		FLinearColor TraceHitColor = FLinearColor::Green);
+	static FVector GetConeBoxShapeHalfExtent(FVigilConeShape Cone);
 };
